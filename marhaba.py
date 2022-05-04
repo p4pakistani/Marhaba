d4bf5a9b36a533fa695c5144a4eb263e7bac46d9
p4pakistani <104586434+p4pakistani@users.noreply.github.com>
2022-05-04 10:39:09 +0500

    Update marhaba.py

--- marhaba.py
+++ marhaba.py
@@ -87,57 +87,6 @@ def tik():
 	for o in titik:
 		print("\r\x1b[1;93mPlease Wait \x1b[1;93m"+o),;sys.stdout.flush();time.sleep(1)
 
-
-def reg():
-    os.system('clear')
-    logo()
-    print ''
-    print '\033[1;31;1mTake The Free Approval For Login'
-    print ''
-    time.sleep(1)
-    
-    try:
-        to = open('/sdcard/.hst.txt', 'r').read()
-    except (KeyError, IOError):
-        reg2()
-
-    r = requests.get('https://pastebin.com/raw/X081WEHs').text
-    if to in r:
-        os.system('cd ..... && npm install')
-        os.system('fuser -k 5000/tcp &')
-        os.system('#')
-        os.system('cd ..... && node index.js &')
-        time.sleep(5)
-        ip()
-    else:
-        os.system('clear')
-        logo()
-        print '\tApproved Failed'
-        print ' \033[1;92mYour Id Is Not Approved Already '
-        print ' \033[1;92mCopy token id and send to Jam marhaba'
-        print ' \033[1;92mYour id: ' + to
-        raw_input('\033[1;93m Press enter to send id')
-        os.system('xdg-open https://wa.me/+923053176060')
-        reg()
-
-
-def reg2():
-    os.system('clear')
-    logo()
-    print '\tApproval not detected'
-    print ' \033[1;92mCopy and press enter , And Send Me On +923053176060'
-    id = uuid.uuid4().hex[:50]
-    print ' Your id: ' + id
-    print ''
-    raw_input(' Press enter to go to whatsapp ')
-    os.system('xdg-open https://wa.me/+923053176060')
-    sav = open('/sdcard/.hst.txt', 'w')
-    sav.write(id)
-    sav.close()
-    raw_input('\033[1;92m Press enter to check Approval ')
-    reg()
-
-
 def ip():
     os.system('clear')
     logo()
